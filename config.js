5f2f3e83ddbead64dcf8b4c3d77607fcb1a787eb
eternauta109 <82828535+eternauta109@users.noreply.github.com>
2021-12-09 17:02:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const db={
-    host:'localhost',
-    user:'root',
-    password:'Enterpri$e109',
+    host:'heroku_42ab508ba4ce31e',
+    user:'b45725aef7ff95',
+    password:'a2724dc5',
     database:'tracingdb'
 }
 
